f55b8f471b0bdb019f90e03304a38c128fb82e4b
PabloTorres <ptorresp@ups.edu.ec>
2025-06-30 16:15:03 -0500

    UPD

---
+++ src/main/Ejercicio_04_depth/Depth.java
@@ -0,0 +1,14 @@
+package main.Ejercicio_04_depth;
+
+import main.Ejercicio_04_depth.Depth;
+import main.Materia.Models.Node;
+
+public class Depth {
+    /// 4 =4
+    // 2=3 7=1
+    // 1=2 3=1
+    // 8 =1
+    public int maxDepth(Node root) {
+        return 0;
+    }
+}
